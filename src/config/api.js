e808d77d8524c6412cf36b701c71ea5ba0c0319c
phyx3r <m.piccardo@gmail.com>
2020-03-31 15:44:23 +0200

    Update api.js
    * fix server host

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   tileLayer: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-  host: 'http://localhost',
+  host: '',
   port: '3000',
-  maxJobNumber: 100
+  maxJobNumber: 1000
 };
